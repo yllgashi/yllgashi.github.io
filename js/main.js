d2e0add80e14ae9bd21dd940d68f3bb6e7e13a35
yllgashi <yll.gashi@keds-energy.com>
2024-04-22 16:00:50 +0200

    re-design profile

--- js/main.js
+++ js/main.js
@@ -1,55 +1,11 @@
-
-// blur profle pic on scrolling down
-let scrollPosition = 0;
-
-window.addEventListener('scroll', () => {
-    if ((document.body.getBoundingClientRect()).top > scrollPosition)
-        document.getElementById('profile-pic').style = "filter: blur(0px);";
-    else document.getElementById('profile-pic').style = "filter: blur(3px);";
-
-    scrollPosition = (document.body.getBoundingClientRect()).top;
+window.addEventListener("load", () => {
+  setFooterDateText();
 });
 
-// rotate profile pic
-window.addEventListener('load', () => {
-    let pic = document.getElementById('profile-pic');
-    pic.style = "transform: rotate(360deg);";
-});
-
-// show "about me" text and call changeRocketPosition method
-let showAboutText = () => {
-    rocketPosition = !rocketPosition;
-    changeRocketPosition();
-
-    document.getElementById('about-hoobies-text').innerHTML = `
-    <p class="about-and-working-text">⭐ I live in Prishtina.</p>
-    <p class="about-and-working-text">🔭 I’m currently working at KEDS.</p>
-    <p class="about-and-working-text">🔥 Eager to learn new technologies.</p>
-    <p class="about-and-working-text">🅰️ I find myself mostly working with: Angular, C# and Transact-SQL.</p>
-    `;
+const setFooterDateText = () => {
+  document.getElementById("date-in-footer").innerText = getCurrentDateInformation();
 };
 
-// show "working on" text and call changeRocketPosition method
-let showWorkingText = () => {
-    rocketPosition = !rocketPosition;
-    changeRocketPosition();
-
-    document.getElementById('about-hoobies-text').innerHTML = `
-    <p class="about-and-working-text">🌖 Angular</p>
-    <p class="about-and-working-text">🌘 C#</p>
-    <p class="about-and-working-text">🌘 Node.js</p>
-    <p class="about-and-working-text">🌘 T-SQL</p>
-    <p class="about-and-working-text">📱 Ionic</p>
-    <p class="about-and-working-text">👀 More</p>
-    `;
+const getCurrentDateInformation = () => {
+  return new Date().toLocaleString("en-us", { month: "short", year: "numeric" });
 };
-
-// rotate rocket
-var rocketPosition = true; // if is true, rocket position is 0degree, else is 180degree
-
-let changeRocketPosition = () => {
-    let rocket = document.getElementById('animated-rocket');
-
-    if (rocketPosition) rocket.style = "transform: rotate(0deg);";
-    else rocket.style = "transform: rotate(180deg);";
-}
